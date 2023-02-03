c3f91a5c93411d8a8c4eecac6532f506343d0b8f
MaAnka324 <matviychuk.anna324@gmail.com>
2023-02-03 14:40:32 +0200

    Change README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Technologies from "./components/Technologies";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
+        <Header/>
+        <Technologies/>
      Hello, samurai! Let's go!
     </div>
   );
