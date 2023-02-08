04ffc9992ca8e53e45ec9f76cd99bbc50393aa10
MaAnka324 <matviychuk.anna324@gmail.com>
2023-02-08 12:48:06 +0200

    Change README.md

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -11,19 +11,19 @@ const Nav = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink to='/profile'>Profile</NavLink>
+                <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/dialogs'>Messages</NavLink>
+                <NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/news'>News</NavLink>
+                <NavLink to='/news' activeClassName={s.active}>News</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/music'>Music</NavLink>
+                <NavLink to='/music' activeClassName={s.active}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/settings'>Settings</NavLink>
+                <NavLink to='/settings' activeClassName={s.active}>Settings</NavLink>
             </div>
         </nav>
     )
