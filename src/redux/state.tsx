c07b161ef20ef4a7ceacc6bae07d07b7fd2b467d
MaAnka324 <matviychuk.anna324@gmail.com>
2023-04-22 15:47:49 +0300

    Change README.md

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -198,8 +198,8 @@ export const store: StoreType = {
     },
     dispatch(action) {
 
-        this._state.profilePage = profileReducer(this._state.profilePage, action)
-        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
+        // this._state.profilePage = profileReducer(this._state.profilePage, action)
+        // this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
         this._state.sidebar = sidebarReducer(this._state.sidebar, action)
 
     }
