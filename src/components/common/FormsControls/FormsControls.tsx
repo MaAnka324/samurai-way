06bd47a3793d92b1c493a0c35b851a05d3c6b687
MaAnka324 <matviychuk.anna324@gmail.com>
2023-09-05 11:20:02 +0300

    Change README.md

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,5 +1,4 @@
 import React, {FC} from 'react';
-import {inspect} from "util";
 import styles from './FormControl.module.css'
 
 
@@ -8,17 +7,12 @@ import styles from './FormControl.module.css'
 //     meta: {}
 // }
 
-export const FormControl = () => {
-
-}
-
-
-export const Textarea: FC<any> = ({input, meta, ...props}) => {
+export const FormControl: FC<any> = ({input, meta, child, ...props}) => {
     const hasError = meta.touched && meta.error
     return (
         <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
             <div>
-                <textarea {...input} {...props} />
+                {props.children}
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
@@ -26,14 +20,17 @@ export const Textarea: FC<any> = ({input, meta, ...props}) => {
 }
 
 
-export const Input: FC<any> = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error
-    return (
-        <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
+export const Textarea: FC<any> = (props) => {
+    const {input, meta, ...restProps} = props
+    return (<FormControl {...props}><textarea {...input} {...restProps}/></FormControl>)
+}
+
+
+// export const Input: FC<any> = ({input, meta, ...props}) => {
+//     return (<FormControl {...props}><input {...input} {...props}/></FormControl>)
+// }
+
+export const Input: FC<any> = (props) => {
+    const {input, meta, ...restProps} = props
+    return (<FormControl {...props}><input {...input} {...restProps}/></FormControl>)
 }
