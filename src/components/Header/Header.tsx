5081073cfb5d3ab90dc11f7e59b5e81c7b0ee1c8
MAO <MAO@d.c>
2024-02-29 12:57:07 +0200

    user small photo header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,6 +11,7 @@ const Header = () => {
     const dispatch = useAppDispatch();
     const isAuth = useAppSelector(state => state.auth.isAuth);
     const login = useAppSelector(state => state.auth.login);
+    const profile = useAppSelector(state => state.profilePage.profile);
 
 
     const logOutHandler = () => {
@@ -19,12 +20,13 @@ const Header = () => {
 
     return (
         <header className={s.header}>
-            <img
-                src='https://abrakadabra.fun/uploads/posts/2022-02/1644169601_3-abrakadabra-fun-p-avatarka-s-ulibkoi-4.jpg'/>
+            {/*<img*/}
+            {/*    src='https://abrakadabra.fun/uploads/posts/2022-02/1644169601_3-abrakadabra-fun-p-avatarka-s-ulibkoi-4.jpg'/>*/}
+            {/*<img alt={'User Photo'} src={profile?.photos.small}/>*/}
             <div className={s.loginBlock}>
                 {isAuth
                     // ? <div>{login} - <button onClick={logOutHandler}>Log Out</button></div>
-                    ? <div className={s.loginName}>{login} - <Button variant="outlined" startIcon={<LogoutIcon />}
+                    ? <div className={s.loginName}><img alt={'User Photo'} src={profile?.photos.small}/> {login}   <Button variant="outlined" startIcon={<LogoutIcon />}
                                              onClick={logOutHandler}>
                         Log Out
                     </Button></div>
