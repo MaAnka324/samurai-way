b8ee2e2260e9daed1397c817fdf3548c9ebee5c3
MAO <MAO@d.c>
2024-02-29 13:02:46 +0200

    nothing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,6 @@ const Header = () => {
     const dispatch = useAppDispatch();
     const isAuth = useAppSelector(state => state.auth.isAuth);
     const login = useAppSelector(state => state.auth.login);
-    const profile = useAppSelector(state => state.profilePage.profile);
 
 
     const logOutHandler = () => {
@@ -20,13 +19,14 @@ const Header = () => {
 
     return (
         <header className={s.header}>
+            <b className={s.nameNetwork}>SOCIAL NETWORK</b>
             {/*<img*/}
             {/*    src='https://abrakadabra.fun/uploads/posts/2022-02/1644169601_3-abrakadabra-fun-p-avatarka-s-ulibkoi-4.jpg'/>*/}
             {/*<img alt={'User Photo'} src={profile?.photos.small}/>*/}
             <div className={s.loginBlock}>
                 {isAuth
                     // ? <div>{login} - <button onClick={logOutHandler}>Log Out</button></div>
-                    ? <div className={s.loginName}><img alt={'User Photo'} src={profile?.photos.small}/> {login}   <Button variant="outlined" startIcon={<LogoutIcon />}
+                    ? <div className={s.loginName}>{login}   <Button variant="outlined" startIcon={<LogoutIcon />}
                                              onClick={logOutHandler}>
                         Log Out
                     </Button></div>
